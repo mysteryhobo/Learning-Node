3e974a88ef91740a1905911d9f0d58467fea022b
mysteryhobo <thepeterlittle@gmail.com>
2016-07-09 16:09:25 -0400

    completed The Node Beginner Book Example

--- The Node Beginner Example Code/server.js
+++ The Node Beginner Example Code/server.js
@@ -1,14 +1,13 @@
 var http = require("http");
 var url = require("url");
 
-function start(route, handle) {
+function start (route, handle) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
-		console.log("Request for " + pathname + " received");
-
-		route(handle, pathname, response);
+		console.log("PathName: " + pathname);
+		route(handle, pathname, response, request);
 	}
-
+	
 	http.createServer(onRequest).listen(8888);
 	console.log("Server has Started");
 }
